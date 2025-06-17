22574b103ed6d06fdff84c01120f01307e44d748
ANHDUY <duyzxje2110@gmail.com>
2025-06-17 09:11:30 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Survey from './pages/surveyInformation';
 import Success from './pages/success';
+import Home from "./pages/home";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/Survey" element={<Survey />} />
         <Route path="/success/:id" element={<Success />} />
       </Routes>
---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+    const navigate = useNavigate();
+    return (
+        <div className="container mt-4">
+            <button className="btn btn-success btn-lg" onClick={() => navigate('/survey')}>Thông tin khảo sát</button>
+        </div>
+    )
+}
+export default Home;
